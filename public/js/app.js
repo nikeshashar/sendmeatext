594a5b71ab9fe6092b7161564c3d137bcdf5b115
Nicolas <nsdcarey@googlemail.com>
2014-11-09 04:28:03 +0000

    big update to public

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+var textApp = angular.module('textApp', [
+'ngRoute',
+'textAppControllers'
+]);
+
+textApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/dialog', {
+        templateUrl: 'partials/dialog.html',
+        controller: 'DialogCtrl'
+      }).
+      when('/dialog/response', {
+        templateUrl: 'partials/dialog-response.html',
+        controller: 'DialogResponseCtrl'
+      }).
+      when('/settings', {
+        templateUrl: 'partials/settings.html',
+        controller: 'SettingsCtrl'
+      }).
+      otherwise({
+        redirectTo: '/dialog'
+      });
+  }]);
---
+++ public/js/controllers.js
@@ -0,0 +1,14 @@
+var textAppControllers = angular.module('textAppControllers', []);
+
+textAppControllers.controller('DialogCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+
+  }]);
+
+textAppControllers.controller('DialogResponseCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+  }]);
+
+textAppControllers.controller('SettingsCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+  }]);
