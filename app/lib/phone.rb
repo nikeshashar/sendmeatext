27a65dc5746db69222debabd75be524a7b1a765e
Tatiana Soukiassian <binaryberry@gmail.com>
2014-11-09 02:06:20 +0000

    testing .gitignore

--- app/lib/phone.rb
+++ app/lib/phone.rb
@@ -1,9 +1,8 @@
 require 'twilio-ruby'
-# require '../../secrets.yml'
+require_relative './secrets.rb'
 
 class Phone
 
-	cf. secrets.yml file (has not been pushed to git intentionnally)
 	attr_accessor :number
 
 	def initialize(number = DEFAULT_NUMBER)
