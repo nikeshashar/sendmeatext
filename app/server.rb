bd0aa27db22c010a0e207f6e558e6f31bca8520d
Tatiana Soukiassian <binaryberry@gmail.com>
2014-11-09 10:35:28 +0000

    Update to data_mapper_setup

--- app/server.rb
+++ app/server.rb
@@ -6,7 +6,7 @@ require_relative './lib/phone'
 
 env = ENV["RACK_ENV"] || "development"
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
-DataMapper.setup(:default, "postgres://localhost/smt3_#{env}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/smt3_#{env}")
 
 require './app/models/message.rb' 
 require './app/models/exchange.rb' 
