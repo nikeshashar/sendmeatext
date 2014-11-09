403b735814e1dbffce0a5e65d1dc5f44176f03c8
Nicolas <nsdcarey@googlemail.com>
2014-11-09 02:01:30 +0000

    new files for public

---
+++ public/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular
+    .module('vineApp', [
+        'vineApp.services',
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute',
+        'chieffancypants.loadingBar',
+        'ngAnimate'
+    ])
+    .config(function ($routeProvider, config) {
+        if (config && config.view === 'settings') {
+            $routeProvider
+                .when('/', {
+                    templateUrl: '/views/settings.html',
+                    controller: 'SettingsCtrl'
+                })
+        } else {
+            $routeProvider
+                .when('/', {
+                    templateUrl: '/views/main.html',
+                    controller: 'MainCtrl'
+                })
+                .when('/settings/', {
+                    templateUrl: '/views/settings.html',
+                    controller: 'SettingsCtrl'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
+        }
+    });
