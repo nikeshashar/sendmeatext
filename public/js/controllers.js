517927f00b423110ebacaee8aeadfc4c266c66ac
Nicolas <nsdcarey@googlemail.com>
2014-11-09 10:20:15 +0000

    changes

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -7,8 +7,13 @@ textAppControllers.controller('DialogCtrl', ['$scope', '$http',
 
 textAppControllers.controller('DialogResponseCtrl', ['$scope', '$routeParams',
   function($scope, $routeParams) {
+
   }]);
 
 textAppControllers.controller('SettingsCtrl', ['$scope', '$routeParams',
   function($scope, $routeParams) {
+  	Wix.UI.initialize({});
+  	$("[wix-model='senderNumber']").getCtrl().setValidationFunction(function(value){
+            return value < 10;
+    });
   }]);
